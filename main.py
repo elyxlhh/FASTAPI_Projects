0dc53040899c7850302a80d31ad653e88d54dc6b
YUZENG LI <ellison0930@gmail.com>
2026-03-22 23:29:05 +0900

    FInish Path Parameters Part

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from class1!")
+from enum import Enum
 
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    main()
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
